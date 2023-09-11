095643e96f01ad935db4935e1b451c02e175e893
onelibero <1792208603@qq.com>
2023-09-11 19:14:11 +0800

    提交了关于聊天信息的

--- src/main/java/cdu/gu/onlinechat/mapper/MessageMapper.java
+++ src/main/java/cdu/gu/onlinechat/mapper/MessageMapper.java
@@ -3,8 +3,14 @@ package cdu.gu.onlinechat.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Mapper
 @Repository
 public interface MessageMapper {
+    int addMessagequn(String name, String message, Date date);
 
+    int deleteMessagequn(String name,String message);
+    int addMessage(String fromid,String toid,Date date);
+    int deleteMessage(String fromid,String toid);
 }
