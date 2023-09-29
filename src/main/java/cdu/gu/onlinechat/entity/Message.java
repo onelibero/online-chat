079c6c406083f32a1266ced462c9e5fbf8c05483
onelibero <1792208603@qq.com>
2023-09-29 23:00:59 +0800

    修改了相关类别

--- src/main/java/cdu/gu/onlinechat/entity/Message.java
+++ src/main/java/cdu/gu/onlinechat/entity/Message.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -12,10 +13,10 @@ import java.util.List;
 @NoArgsConstructor
 public class Message {
 
-    private String from;  //发送者
-    private String to;        //接受方
-    private String sendDate ; //发送日期
-    private String content;   //发送内容
+    private String fromid;      //发送者
+    private String toid;        //接受方
+    private Date sendDate ; //发送日期
+    private String messgae;   //发送内容
     private String messageType;//发送消息类型（“text”文本，“image”图片）
     private List<String> userNames;  //
 }
